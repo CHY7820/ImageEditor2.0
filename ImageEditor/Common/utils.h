cedb2987261ee4023bc3b6c69a2cb8a9ab704032
CHY7820 <53003548+CHY7820@users.noreply.github.com>
2020-07-18 00:37:37 +0800

    clear

--- ImageEditor/Common/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-#include <opencv2/opencv.hpp>
-#include <QImage>
-
-QImage Mat2QImage(const cv::Mat &src);
-cv::Mat QImage2Mat(const QImage &src);
-
-#endif // UTILS_H
